fcbd44a3c4d76716ba2e53aa240bb0b7d8b31e8a
unknown <BT_4N2_07@BKTECH.LOCAL>
2023-10-02 13:59:15 -0400

    Initial commit

---
+++ src/Runner.java
@@ -0,0 +1,84 @@
+public class Runner {
+    public static void main(String[] args) {
+        /*
+        Rectangle rect1 = new Rectangle(80, 100);
+        Rectangle rect2 = new Rectangle(50);
+        Rectangle rect3 = new Rectangle();
+        rect3 = rect1;
+        rect1 = rect2;
+        rect1.setLength(60);
+        Rectangle rect4 = rect3;
+        rect3.setWidth(70);
+        rect3 = rect1;
+        rect3.setLength(40);
+        rect2.setWidth(90);
+        System.out.println(rect1.area());
+        System.out.println(rect2.area());
+        System.out.println(rect3.area());
+        System.out.println(rect4.area());
+        */
+
+        /*
+        Rectangle rectA = new Rectangle(80, 100);
+        Rectangle rectB = new Rectangle(80, 100);
+        boolean areEqual = (rectA == rectB);
+        System.out.println(areEqual);
+        */
+
+        /*
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = r1;
+        r1 = null;
+        r2.printRectangleInfo();
+        */
+        /*
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = r1;
+        r2 = null;
+        r1.printRectangleInfo();
+        */
+        /*
+        Rectangle r1 = new Rectangle();
+        Rectangle r2 = r1;
+        r1 = null;
+        r1.printRectangleInfo();
+        */
+
+        /*
+        ShoppingListItem item;
+        ShoppingListItem item2 = new ShoppingListItem("apple", 0.50, 10);
+        item = item2;
+        double total = item.totalCost();
+        */
+        /*
+        ShoppingListItem item = new ShoppingListItem("carrot", 20, 0.25);
+        double total = item.totalCost();
+        */
+        /*
+        ShoppingListItem item = new ShoppingListItem("apple", 0.50, 10);
+        ShoppingListItem item2 = new ShoppingListItem("banana", 0.75, 5);
+        item.increaseQuantity(7);
+        item2.increaseQuantity(4);
+        double total = item.totalCost() + item2.totalCost();
+        */
+        /*
+        ShoppingListItem item = null;
+        String s = item.toString();
+        item = new ShoppingListItem("apple", 0.50, 10);
+        double total = item.totalCost();
+        */
+
+        /*
+        ShoppingListItem item = new ShoppingListItem("apple", 0.50, 10);
+        item.toString();
+        */
+        /*
+        ShoppingListItem item = new ShoppingListItem("apple", 0.50, 10);
+        System.out.println(item.toString());
+        */
+        /*
+        ShoppingListItem item = new ShoppingListItem("apple", 0.50, 10);
+        System.out.println(item);
+        */
+    }
+}
